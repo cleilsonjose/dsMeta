97eaf2cb4fda168d47ac5530fd41bef365aaf6bb
cleilsonjose <cleilsonrock@gmail.com>
2022-09-07 19:37:17 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificationButton from './components/NotificationButton'
+
 function App() {
   return(
-    <h1>oi</h1>
+    <>
+       <h1>oi</h1>
+       <NotificationButton/>
+       <NotificationButton/>
+    </>
+   
+    
   )
 }
 
